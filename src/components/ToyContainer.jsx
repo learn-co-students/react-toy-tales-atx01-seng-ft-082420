486ab8f7e14009259ad4c383c2f8cc518170e13a
devindavis5 <devinrdavis555@gmail.com>
2020-10-29 15:07:38 -0500

    Done.

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+const ToyContainer = (props) => {
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {props.toys.map(toy => (
+        <ToyCard toy={toy} key={toy.id} id={toy.id} name={toy.name} img={toy.image} likes={toy.likes} deleteToy={props.deleteToy} addLike={props.addLike}/>
+      ))}
     </div>
   );
 }
