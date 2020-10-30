5e8877ed4d1092902d9cbc8391f1390c275a2130
jlonetree <jlonetree@gmail.com>
2020-10-30 09:15:37 -0500

    Done.

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
-  return(
+const ToyContainer = (props) => {
+  console.log("this is working", props.toys)
+  return (
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {props.toys.map(toy => {
+        return (
+          <ToyCard 
+            toy = {toy}
+            key = {toy.id}
+            name = {toy.name}
+            image = {toy.image}
+            likes = {toy.likes}
+            updateLikes = {props.updateLikes}
+            donateToy = {props.donateToy}
+          />
+        )
+      })}
+      {/* <ToyCard updateLikes = {props.updateLikes}/> */}
     </div>
-  );
+  )
 }
 
 export default ToyContainer;
